73df4dadde9717ecfbe3907d597e02c32ea756af
ArisPutraBuana <arispb99@gmail.com>
2020-03-26 23:40:40 +0700

    -

---
+++ application/views/frontend/footer.php
@@ -0,0 +1,13 @@
+<footer id="footer">
+
+            <!-- FOOTER BOTTOM -->
+            <div class="footer_bottom">
+                <div class="container">
+                    <p>&copy; <?= date('Y');?> A Hotel All rights reserved | Developed By 
+                    <a href="https://ArisPutraBuana" target="_blank" style="color: #fff;">Aris Putra Buana</a> 
+                    </p>
+                </div>
+            </div>
+            <!-- END / FOOTER BOTTOM -->
+
+        </footer>
